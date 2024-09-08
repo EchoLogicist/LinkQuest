1c3b927b99f8656d4be8a5d7c6dffdf28b1a18aa
hemanth.rajanna <hemanth.rajanna@starmarkit.com>
2024-09-08 20:35:38 +0530

    .

--- linkQuest-server/Hub/communicationHub.cs
+++ linkQuest-server/Hub/communicationHub.cs
@@ -10,7 +10,7 @@ namespace linkQuest_server
     {
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessages", user, message);
         }
     }
 }
